8f9bf0f989f0b657cc60b5a1657710705b4ce242
gayatri-shelke <89582957+gayatri-shelke@users.noreply.github.com>
2022-04-18 20:48:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import From from './component/From'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      {/* <img src='human.jpg'></img> */}
+      <From/>
+      
+     
+    </div>
+
+  );
+}
+
+export default App;
